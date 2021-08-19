7de61bcb3a75e0d8064cf32acd8c5c97bea7c449
krishmanicks <krishanthmanicks@gmail.com>
2021-08-19 21:16:58 +0530

    largest number formed from an array

--- arrays/LARGESTNOARRAY.C
+++ arrays/LARGESTNOARRAY.C
@@ -1,28 +1,43 @@
 #include<stdio.h>
 
+int merge(int a, int b)
+{
+    int length = 0;
+    int temp = b;
+    while(b)
+    {
+        length++;
+        b = b/10;
+    }
+    while(length)
+    {
+        a = a*10;
+        length--;
+    }
+    return (a + temp);
+}
+
 int main()
 {
-    int n = 5;
-    int a[] = {3, 30, 34, 5, 9};
+    int n = 8;
+    int arr[] = {1, 34, 3, 98, 9, 76, 45, 4};
     int i=0,j=1;
 
-    for(i=0;i<=n-2;i++)
+    for(i=0;i<n-1;i++)
     { 
-        j = i+1;
-       while(j<n)
-        {
-            if(a[i],a[j] > a[j],a[i]) 
-                {
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                    break;
-                }                          
-                    j++;
-        } 
-    }   
+       for(int j=i+1;j<n;j++)
+       {
+          if(merge(arr[i],arr[j])  < merge(arr[j],arr[i]))
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+       }
+    }
+
     for(i=0;i<n;i++)
     {
-        printf("%d ", a[i]);
+        printf("%d", arr[i]);
     }   
 }
